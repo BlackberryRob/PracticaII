83cdb4a9925550f98051c61eecce78ec8767bcbf
BlackberryRob <Rob.mora@outlook.com>
2025-02-01 13:40:32 -0600

    Primer commit de la practica II

---
+++ src/test/java/com/practicaII/practicaII/PracticaIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicaII.practicaII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
